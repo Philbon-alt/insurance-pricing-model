e52d0a0223f2326add0712f48812ac333ebde7df
philbon-alt <phil_bobo@live.fr>
2025-06-05 02:54:09 -0400

    Updating (and completing) the LM script.

--- R/LinearModel.R
+++ R/LinearModel.R
@@ -1,24 +1,39 @@
-train_ds <- read.csv("data/train_auto.csv")
-test_ds <- read.csv("data/test_auto.csv")
+rm(list = ls())
+
+df <- read.csv("data/train_auto.csv")
+source('R/fonction.R')
+
+#This ds contains some NULL/empty, I'm gonna relace them with NA
+df[df == ""] <- NA
 
 # For this first model, we will ignore any row that has a missing value
-train_ds <- na.omit(train_ds)
+df <- na.omit(df)
 
 # Convert categorical variables to factors
-train_ds$PARENT1 <- as.factor(train_ds$PARENT1)
-train_ds$MSTATUS <- as.factor(train_ds$MSTATUS)
-train_ds$SEX <- as.factor(train_ds$SEX)
-train_ds$EDUCATION <- as.factor(train_ds$EDUCATION)
-train_ds$JOB <- as.factor(train_ds$JOB)
-train_ds$CAR_TYPE <- as.factor(train_ds$CAR_TYPE)
-train_ds$CAR_USE <- as.factor(train_ds$CAR_USE)
-train_ds$RED_CAR <- as.factor(train_ds$RED_CAR)
-train_ds$REVOKED <- as.factor(train_ds$REVOKED)
-train_ds$URBANICITY <- as.factor(train_ds$URBANICITY)
+df$PARENT1 <- as.factor(df$PARENT1)
+df$MSTATUS <- as.factor(df$MSTATUS)
+df$SEX <- as.factor(df$SEX)
+df$EDUCATION <- as.factor(df$EDUCATION)
+df$JOB <- as.factor(df$JOB)
+df$CAR_TYPE <- as.factor(df$CAR_TYPE)
+df$CAR_USE <- as.factor(df$CAR_USE)
+df$RED_CAR <- as.factor(df$RED_CAR)
+df$REVOKED <- as.factor(df$REVOKED)
+df$URBANICITY <- as.factor(df$URBANICITY)
 
+# Convert the Monetary values to a numeric variable
+df$INCOME <- as.numeric(gsub("[$,]", "", df$INCOME))
+df$HOME_VAL <- as.numeric(gsub("[$,]", "", df$HOME_VAL))
+df$BLUEBOOK <- as.numeric(gsub("[$,]", "", df$BLUEBOOK))
+df$OLDCLAIM <- as.numeric(gsub("[$,]", "", df$OLDCLAIM))
 
 # "Clean" the data by removing non-variable columns
-train_ds <- train_ds[, -c(1, 2)]
+df <- df[, -c(1, 2)]
+
+# Split the dataset into training and test datasets
+ListSubset<-train_test_split(0.8, df)
+train_ds <- ListSubset[[1]]
+test_ds <- ListSubset[[2]]
 
 # Linear Regression Model
 lm_model <- lm(TARGET_AMT ~ ., data = train_ds)
@@ -26,3 +41,5 @@ lm_model <- lm(TARGET_AMT ~ ., data = train_ds)
 # Summary of the model
 summary(lm_model)
 
+# Save the model
+saveRDS(lm_model, file = "models/LinearModel.rds")
