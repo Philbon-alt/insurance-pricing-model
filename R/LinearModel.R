ec0b7d73277c79dbdc8be6963884c55e613c8386
philbon-alt <phil_bobo@live.fr>
2025-06-20 20:10:10 -0400

    format code

--- R/LinearModel.R
+++ R/LinearModel.R
@@ -1,11 +1,11 @@
 rm(list = ls())
 
 source('R/fonction.R')
-df_train<-readRDS('data/clean_train.rds')
-df_test<-readRDS('data/clean_test.rds')
+df_train <- readRDS('data/clean_train.rds')
+df_test <- readRDS('data/clean_test.rds')
 
 # Removing non-variable columns
-df_train <- df_train[, -c(1, 2)]
+df_train <- df_train[,-c(1, 2)]
 
 # Linear Regression Model
 lm_model <- lm(TARGET_AMT ~ ., data = df_train)
@@ -14,8 +14,8 @@ lm_model <- lm(TARGET_AMT ~ ., data = df_train)
 summary(lm_model)
 
 # Save the model
-pred<-predict.lm(lm_model, df_test)
-LM_EQM<-EQM(pred, df_test$p_target)
+pred <- predict.lm(lm_model, df_test)
+LM_EQM <- EQM(pred, df_test$p_target)
 LM_EQM
 
 saveRDS(lm_model, file = "models/LinearModel.rds")
