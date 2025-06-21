164d926477916c1d62729d07d9547c841ed21a4e
Philippe Thibodeau <98897411+Philbon-alt@users.noreply.github.com>
2025-06-21 12:13:54 -0400

    Update GammaPoisson.R
    Preparing file for importing already cleaned data

--- R/GammaPoisson.R
+++ R/GammaPoisson.R
@@ -8,27 +8,6 @@ source('R/fonction.R')
 #This ds contains some NULL/empty, I'm gonna relace them with NA
 df[df == ""] <- NA
 
-# For this first model, we will ignore any row that has a missing value
-df <- na.omit(df)
-
-# Convert categorical variables to factors
-df$PARENT1 <- as.factor(df$PARENT1)
-df$MSTATUS <- as.factor(df$MSTATUS)
-df$SEX <- as.factor(df$SEX)
-df$EDUCATION <- as.factor(df$EDUCATION)
-df$JOB <- as.factor(df$JOB)
-df$CAR_TYPE <- as.factor(df$CAR_TYPE)
-df$CAR_USE <- as.factor(df$CAR_USE)
-df$RED_CAR <- as.factor(df$RED_CAR)
-df$REVOKED <- as.factor(df$REVOKED)
-df$URBANICITY <- as.factor(df$URBANICITY)
-
-# Convert the Monetary values to a numeric variable
-df$INCOME <- as.numeric(gsub("[$,]", "", df$INCOME))
-df$HOME_VAL <- as.numeric(gsub("[$,]", "", df$HOME_VAL))
-df$BLUEBOOK <- as.numeric(gsub("[$,]", "", df$BLUEBOOK))
-df$OLDCLAIM <- as.numeric(gsub("[$,]", "", df$OLDCLAIM))
-
 # "Clean" the data by removing non-variable columns
 df <- df[, -c(1, 2)]
 
