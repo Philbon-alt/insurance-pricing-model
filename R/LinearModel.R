79ca8f9829620de403f9158d4ce599eee8814272
philbon-alt <phil_bobo@live.fr>
2025-06-27 18:05:02 -0400

    added a sub-model that takes NA categories

--- R/LinearModel.R
+++ R/LinearModel.R
@@ -19,3 +19,22 @@ LM_EQM <- EQM(pred, df_test$p_target)
 LM_EQM
 
 saveRDS(lm_model, file = "models/LinearModel.rds")
+
+# Now let's try a model that takes the dataset where the NAs are categories
+df_train_na <- readRDS('data/clean_train_na.rds')
+df_test_na <- readRDS('data/clean_test_na.rds')
+
+# Removing non-variable columns
+df_train_na <- df_train_na[,-c(1, 2)]
+
+# Linear Regression Model with NAs as categories
+lm_model_na <- lm(TARGET_AMT ~ ., data = df_train_na)
+
+# Summary of the model with NAs as categories
+summary(lm_model_na)
+
+# Let's try the precision now
+pred_na <- predict.lm(lm_model_na, df_test_na)
+LM_EQM_na <- EQM(pred_na, df_test_na$p_target)
+LM_EQM_na < LM_EQM
+
