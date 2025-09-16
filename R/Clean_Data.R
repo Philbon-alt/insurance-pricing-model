6e0c7cc290abf8cee2e7723eb1dd5eda233c8a21
philbon-alt <phil_bobo@live.fr>
2025-09-16 13:53:36 -0400

    Modified the datas since last one was comparing p_values with claim

--- R/Clean_Data.R
+++ R/Clean_Data.R
@@ -5,7 +5,19 @@ df <- read.csv("data/train_auto.csv")
 #This ds contains some NULL/empty, I'm gonna relace them with NA
 df[df == ""] <- NA
 
-# For this first model, we will ignore any row that has a missing value
+#Replace missing categories with "Missing"
+cat_vars <- c("PARENT1", "MSTATUS", "SEX", "EDUCATION", "JOB",
+              "CAR_TYPE", "CAR_USE", "RED_CAR", "REVOKED", "URBANICITY")
+
+for (var in cat_vars) {
+  if (var %in% names(df)) {
+    df[[var]] <- as.character(df[[var]])
+    df[[var]][is.na(df[[var]])] <- "Missing"
+    df[[var]] <- as.factor(df[[var]])
+  }
+}
+
+#Remove the remaining NAs (rows with NAs in numeric variables)
 df <- na.omit(df)
 
 # Convert categorical variables to factors
@@ -26,12 +38,52 @@ df$HOME_VAL <- as.numeric(gsub("[$,]", "", df$HOME_VAL))
 df$BLUEBOOK <- as.numeric(gsub("[$,]", "", df$BLUEBOOK))
 df$OLDCLAIM <- as.numeric(gsub("[$,]", "", df$OLDCLAIM))
 
-# Save this as Clean data.
+#Since they are in order of INDEX, there is no need to shuffle the data
+ntest<-round(nrow(df)/4,0)
 
-saveRDS(df, file = "data/clean_train.rds")
+clean_test_na <- df[0:ntest, ]
+clean_train_na <- df[(ntest+1):nrow(df), ]
 
+# Save this as na data.
+saveRDS(clean_train_na, file = "data/clean_train_na.rds")
+saveRDS(clean_test_na, file = "data/clean_test_na.rds")
+
+# Now let's make the clean data without NA categories
+df_noNA <- read.csv("data/train_auto.csv")
+df_noNA[df_noNA == ""] <- NA
+df_noNA <- na.omit(df_noNA)
+
+# Convert categorical variables to factors
+df_noNA$PARENT1 <- as.factor(df_noNA$PARENT1)
+df_noNA$MSTATUS <- as.factor(df_noNA$MSTATUS)
+df_noNA$SEX <- as.factor(df_noNA$SEX)
+df_noNA$EDUCATION <- as.factor(df_noNA$EDUCATION)
+df_noNA$JOB <- as.factor(df_noNA$JOB)
+df_noNA$CAR_TYPE <- as.factor(df_noNA$CAR_TYPE)
+df_noNA$CAR_USE <- as.factor(df_noNA$CAR_USE)
+df_noNA$RED_CAR <- as.factor(df_noNA$RED_CAR)
+df_noNA$REVOKED <- as.factor(df_noNA$REVOKED)
+df_noNA$URBANICITY <- as.factor(df_noNA$URBANICITY)
+
+# Convert the Monetary values to a numeric variable
+df_noNA$INCOME <- as.numeric(gsub("[$,]", "", df_noNA$INCOME))
+df_noNA$HOME_VAL <- as.numeric(gsub("[$,]", "", df_noNA$HOME_VAL))
+df_noNA$BLUEBOOK <- as.numeric(gsub("[$,]", "", df_noNA$BLUEBOOK))
+df_noNA$OLDCLAIM <- as.numeric(gsub("[$,]", "", df_noNA$OLDCLAIM))
+
+# Again, since these are index, no need to shuffle
+ntestnoNA<-round(nrow(df_noNA)/4,0)
+
+clean_test <- df_noNA[0:ntestnoNA, ]
+clean_train <- df_noNA[(ntestnoNA+1):nrow(df_noNA), ]
+
+# Save this as Clean data without NA categories
+saveRDS(clean_train, file = "data/clean_train.rds")
+saveRDS(clean_test, file = "data/clean_test.rds")
+
+# Let's do the test set, just to test the mean since we don't have the target amount
 df2 <- read.csv("data/test_auto.csv")
-df3 <- read.csv("data/SHELL_AUTO.csv")
+df3 <- read.csv("data/MEAN_AUTO.csv")
 
 merged <- merge.data.frame(df2, df3, by="INDEX")
 
@@ -58,49 +110,12 @@ merged[merged == ""] <- NA
 
 merged <- na.omit(merged)
 
-saveRDS(merged, file = "data/clean_test.rds")
+saveRDS(merged, file = "data/mean_test.rds")
 
-# Make dataset where we have NA categories for the factor variables
-
-df_na <- read.csv("data/train_auto.csv")
-df_na[df_na == ""] <- NA
-
-cat_vars <- c("PARENT1", "MSTATUS", "SEX", "EDUCATION", "JOB",
-              "CAR_TYPE", "CAR_USE", "RED_CAR", "REVOKED", "URBANICITY")
-
-for (var in cat_vars) {
-  if (var %in% names(df_na)) {
-    df_na[[var]] <- as.character(df_na[[var]])
-    df_na[[var]][is.na(df_na[[var]])] <- "Missing"
-    df_na[[var]] <- as.factor(df_na[[var]])
-  }
-}
-
-df_na <- na.omit(df_na)
-
-# Convert categorical variables to factors
-df_na$PARENT1 <- as.factor(df_na$PARENT1)
-df_na$MSTATUS <- as.factor(df_na$MSTATUS)
-df_na$SEX <- as.factor(df_na$SEX)
-df_na$EDUCATION <- as.factor(df_na$EDUCATION)
-df_na$JOB <- as.factor(df_na$JOB)
-df_na$CAR_TYPE <- as.factor(df_na$CAR_TYPE)
-df_na$CAR_USE <- as.factor(df_na$CAR_USE)
-df_na$RED_CAR <- as.factor(df_na$RED_CAR)
-df_na$REVOKED <- as.factor(df_na$REVOKED)
-df_na$URBANICITY <- as.factor(df_na$URBANICITY)
-
-# Convert the Monetary values to a numeric variable
-df_na$INCOME <- as.numeric(gsub("[$,]", "", df_na$INCOME))
-df_na$HOME_VAL <- as.numeric(gsub("[$,]", "", df_na$HOME_VAL))
-df_na$BLUEBOOK <- as.numeric(gsub("[$,]", "", df_na$BLUEBOOK))
-df_na$OLDCLAIM <- as.numeric(gsub("[$,]", "", df_na$OLDCLAIM))
-
-# Save this as Clean data with NA categories
-saveRDS(df_na, file = "data/clean_train_na.rds")
+# Make one for the test set with NA categories
 
 df_na2 <- read.csv("data/test_auto.csv")
-df_na3 <- read.csv("data/SHELL_AUTO.csv")
+df_na3 <- read.csv("data/MEAN_AUTO.csv")
 merged_na <- merge.data.frame(df_na2, df_na3, by="INDEX")
 merged_na <- merged_na[,-c(1,2,3)]
 
@@ -136,4 +151,4 @@ merged_na$OLDCLAIM <- as.numeric(gsub("[$,]", "", merged_na$OLDCLAIM))
 # Save this as Clean data with NA categories
 merged_na <- na.omit(merged_na)
 
-saveRDS(merged_na, file = "data/clean_test_na.rds")
+saveRDS(merged_na, file = "data/mean_test_na.rds")
